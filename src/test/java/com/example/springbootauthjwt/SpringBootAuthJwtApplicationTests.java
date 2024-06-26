0b85e931001d255840532c2f616a21c449bd11f2
arman <armanruhit09@gmail.com>
2024-06-26 16:05:04 +0600

    init

---
+++ src/test/java/com/example/springbootauthjwt/SpringBootAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootauthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
